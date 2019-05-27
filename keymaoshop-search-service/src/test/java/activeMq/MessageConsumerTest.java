72da668c65e8e9da93c14cdb6d97b184eb5f3ffa
DaMingMing <1044096131@qq.com>
2019-05-27 23:57:08 +0800

    activeMq跟Spring 整合测试类

---
+++ keymaoshop-search-service/src/test/java/activeMq/MessageConsumerTest.java
@@ -0,0 +1,16 @@
+package activeMq;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MessageConsumerTest {
+    @Test
+    public void testQueueConsumer() throws Exception {
+/*        //初始化spring容器
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-activeMq.xml");
+        //等待
+        System.in.read();*/
+    }
+
+}
